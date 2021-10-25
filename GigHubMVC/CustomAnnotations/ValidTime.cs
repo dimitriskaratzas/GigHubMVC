de45217021bdda1dd5ada97cd009b9feb27d6e32
Dimitris Karatzas <dimkaratz85@gmail.com>
2021-10-25 19:43:25 +0300

    Implementing Validation

---
+++ GigHubMVC/CustomAnnotations/ValidTime.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace GigHubMVC.CustomAnnotations
+{
+    public class ValidTime : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            DateTime Time;
+            var isValid = DateTime.TryParseExact(Convert.ToString(value), 
+                "HH:mm",
+                DateTimeFormatInfo.InvariantInfo, DateTimeStyles.None,
+                out Time);
+
+            return (isValid);
+        }
+        
+    }
+}
--- GigHubMVC/ViewModels/GigFormViewModel.cs
+++ GigHubMVC/ViewModels/GigFormViewModel.cs
@@ -1,14 +1,33 @@
-﻿using GigHubMVC.Models;
+﻿using GigHubMVC.CustomAnnotations;
+using GigHubMVC.Models;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace GigHubMVC.ViewModels
 {
     public class GigFormViewModel
     {
+        [Required]
         public string Venue { get; set; }
+
+        [Required]
+        [FutureDate]
         public string Date { get; set; }
+
+        [Required]
+        [ValidTime(ErrorMessage ="The time formula is invalid")]
         public string Time { get; set; }
+
+        [Required]
         public byte Genre { get; set; } //this is the id
+
         public IEnumerable<Genre> Genres { get; set; }
+
+        public DateTime GetDateTime()
+        {
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time));
+            
+        }
     }
 }
