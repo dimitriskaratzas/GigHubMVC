8fcfc6c4d3072a33f8ce2f4de630089938916334
Dimitris Karatzas <dimkaratz85@gmail.com>
2021-10-29 22:02:00 +0300

    Added Cancelling feature (Basic)

---
+++ GigHubMVC/Controllers/Api/GigsController.cs
@@ -0,0 +1,35 @@
+﻿using GigHubMVC.Models;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Http;
+
+namespace GigHubMVC.Controllers.Api
+{
+    [Authorize]
+    public class GigsController : ApiController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Cancel(int id)
+        {
+            var userId = User.Identity.GetUserId();
+
+            var gig = _context.Gigs
+                .SingleOrDefault(g => g.ID == id && g.ArtistId == userId);
+
+            if (gig == null)
+                return NotFound();
+
+            gig.IsCancelled = true;
+
+            _context.SaveChanges();
+            return Ok();
+        }
+    }
+}
--- GigHubMVC/Models/Gig.cs
+++ GigHubMVC/Models/Gig.cs
@@ -7,6 +7,7 @@ namespace GigHubMVC.Models
     {
         public int ID { get; set; }
 
+        public bool IsCancelled { get; set; }
         
         public ApplicationUser Artist { get; set; }
 
