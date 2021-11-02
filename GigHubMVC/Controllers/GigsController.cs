2fffb1dd1b0744f31dc3341fb3dddf6339031132
Dimitris Karatzas <dimkaratz85@gmail.com>
2021-11-02 18:49:51 +0200

    Add Details Method on GigsController

--- GigHubMVC/Controllers/GigsController.cs
+++ GigHubMVC/Controllers/GigsController.cs
@@ -31,6 +31,31 @@ namespace GigHubMVC.Controllers
             return View(gigs);
         }
 
+        public ActionResult Details(int id)
+        {
+            var gig = _context.Gigs
+                .Include(g => g.Artist)
+                .Include(g => g.Genre)
+                .SingleOrDefault(g => g.ID == id);
+
+            if (gig == null)
+                return HttpNotFound();
+
+            var viewModel = new GigDetailsViewModel { Gig = gig };
+            if (User.Identity.IsAuthenticated)
+            {
+                var userId = User.Identity.GetUserId();
+
+                viewModel.IsAttending = _context.Attendances
+                    .Any(a => a.GigId == gig.ID && a.AttendeeId == userId);
+
+                viewModel.IsFollowing = _context.Followings
+                    .Any(f => f.FolloweeId == gig.ArtistId && f.FollowerId == userId);
+            }
+
+            return View("Details", viewModel);
+        }
+
         [Authorize]
         public ActionResult Attending()
         {
